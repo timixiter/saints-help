e2ce6ea5b6ffefd0e5643b299450c9c7479d800c
MIWATEAM <riziqa96@gmail.com>
2026-07-06 18:03:20 +0700

    Delete api directory

--- api/logout.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  if (req.method === 'OPTIONS') return res.status(204).end();
-  return res.status(200).json({ success: true });
-}
